e57f1dbb8fe34e3c64b9ee58b014cf142a4e7fb6
Ariel Nadratowski <Ariel_nadratowski@turnerdesigns.com>
2024-04-10 15:48:50 -0700

    adding frame to show warnings generated

--- Bom_Checker_Main.py
+++ Bom_Checker_Main.py
@@ -23,23 +23,40 @@ class BomCheckerMainWindow(tk.Tk):
         self.test_name.pack(side = "top")
 
         # set up frame for buttons you interact with
-        input_frame = tk.Frame(self, width = 1400, height = 200)
+        input_frame = tk.Frame(self)
         input_frame.pack(side = "top")
 
         self.bomA_label_value = tk.StringVar(input_frame, "Please upload file")
-        self.bomA_button = tk.Button(input_frame, text = "upload BOM A", command = self.uploadFileA)
+        bomA_button = tk.Button(input_frame, text = "upload BOM A", command = self.uploadFileA)
         self.bomA_label = tk.Label(input_frame, textvariable=self.bomA_label_value)
-        self.bomA_button.grid(row = 0, column = 0)
-        self.bomA_label.grid(row = 0, column = 1)
+        bomA_button.grid(row = 0, column = 0)
+        self.bomA_label.grid(row = 0, column = 1, padx = 5, pady = 5)
 
         self.bomB_label_value = tk.StringVar(input_frame, "Please upload file")
-        self.bomB_button = tk.Button(input_frame, text = "upload BOM B", command = self.uploadFileB)
+        bomB_button = tk.Button(input_frame, text = "upload BOM B", command = self.uploadFileB)
         self.bomB_label = tk.Label(input_frame, textvariable=self.bomB_label_value)   
-        self.bomB_button.grid(row = 1, column = 0)
-        self.bomB_label.grid(row = 1, column = 1)
+        bomB_button.grid(row = 1, column = 0)
+        self.bomB_label.grid(row = 1, column = 1, padx = 5, pady = 5)
 
-        self.compare_button = tk.Button(input_frame, text = "COMPARE BOMs", command = compareBoms)
-        self.compare_button.grid(row = 3, column = 0, columnspan = 2)
+        compare_button = tk.Button(input_frame, text = "COMPARE BOMs", command = compareBoms)
+        compare_button.grid(row = 3, column = 0, columnspan = 2, padx = 10, pady = 10)
+
+        # make frame to display the warnings
+        warnings_frame = tk.Frame(self)
+        warnings_frame.pack(side = "top", padx = 10, pady = 10)
+        warnings_frame.pack_propagate(False)
+        
+        warnings_row = []
+        warnings_list = ["warning1", "warning2", "warning3"]
+        for index, value in enumerate(warnings_list):
+            warnings_row.append(ttk.Label(warnings_frame, text = value))
+            warnings_row[index].grid()
+
+        # display the flagged rows from the BOMs
+        
+
+
+
 
     def uploadFileA(self, *_):
         filename = filedialog.askopenfilename()
@@ -50,10 +67,10 @@ class BomCheckerMainWindow(tk.Tk):
         filename = filedialog.askopenfilename()
         print('Selected:', filename)
         self.bomB_label_value.set(filename)
+        
 
 
-        warnings_frame = tk.Frame(self, background = "grey", width = 1400, height = 200)
-        warnings_frame.pack(side = "top")
 
+# https://stackoverflow.com/questions/44798950/how-to-display-a-dataframe-in-tkinter
 
 main()
